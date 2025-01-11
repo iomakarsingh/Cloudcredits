6fce9c5f3ee7af6985f3d95b2bca0b8e19c16953
Omakar Singh <152642916+iomakarsingh@users.noreply.github.com>
2025-01-11 20:57:30 +0530

    Add files via upload

---
+++ Chat_Application/src/main/java/com/chat/Launcher.java
@@ -0,0 +1,9 @@
+package  com.chat;
+
+import com.chat.server.ServerLauncher;
+
+public class Launcher {
+    public static void main(String[] args) {
+        ServerLauncher.main(new String[]{});
+    }
+}
