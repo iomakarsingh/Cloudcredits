77c72d83577a0286261739a7616323eb7719859d
Omakar Singh <152642916+iomakarsingh@users.noreply.github.com>
2025-01-11 20:55:38 +0530

    Add files via upload

---
+++ Chat_Application/src/main/java/com/chat/client/ClientLauncher.java
@@ -0,0 +1,34 @@
+package com.chat.client;
+
+import java.io.IOException;
+
+import com.chat.controller.ClientFormController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ClientLauncher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ClientForm.fxml"));
+        ClientFormController controller = new ClientFormController();
+        fxmlLoader.setController(controller);
+        primaryStage.setScene(new Scene(fxmlLoader.load()));
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(primaryStage.getScene().getWindow());
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        stage.setTitle("EChat");
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
+}
